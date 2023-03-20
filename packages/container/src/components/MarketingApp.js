a8aedefda3908ba5f8fa89bdf38b556deff7a290
riccardo andronache <dev.riccardo.a@gmail.com>
2023-03-20 16:56:39 +0200

    added navigation both ways, container to subapp and subapp to container

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,25 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
+import { mount } from "marketing/MarketingApp";
+import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  });
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: (location) => {
+        const { pathname: currPathname } = history.location;
+        const { pathname: nextPathname } = location;
+
+        if (currPathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
